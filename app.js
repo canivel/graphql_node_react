28f53067110ef5fa36fa966ab28e87a0cf7a7460
dcanivel-at-325274068097 <IESzg7L/tD0VMsuy/YKjuPgawtjynvmwD+rO24MtpGs=>
2018-12-13 20:11:29 -0500

    add mongodb

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
-
+const mongoose = require("mongoose");
+const Event = require("./models/event");
 const app = express();
+
+// const events = [];
 
 app.use(bodyParser.json());
 
@@ -11,12 +14,27 @@ app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
+      type Event{
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
       type RootQuery{
         events: [String!]!
       }
 
       type RootMutation{
-        createEvent(name: String): String
+        createEvent(eventIput: EventInput): String
       }
 
       schema {
@@ -26,11 +44,41 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ["Event 1", "Event 2", "Event 3"];
+        Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
       createEvent: args => {
-        const eventName = args.name;
-        return eventName;
+        // const event = {
+        //   _id: Math.random().toString(),
+        //   title: args.eventIput.title,
+        //   description: args.eventIput.description,
+        //   price: +args.eventIput.price,
+        //   date: args.eventIput.date
+        // };
+        const event = new Event({
+          title: args.eventIput.title,
+          description: args.eventIput.description,
+          price: +args.eventIput.price,
+          date: new Date(args.eventIput.date)
+        });
+        // events.push(event);
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: event.id };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
       }
     },
     graphiql: true
@@ -41,4 +89,15 @@ app.get("/", (req, res, next) => {
   res.send("Hello Wordl");
 });
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-bqhq9.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
